ba47bbb935392b96ffd9f34295fe7de46391b6df
Itay Maman <imaman@users.noreply.github.com>
2019-08-25 19:30:58 +0300

    lambdaclient

---
+++ packages/lambda/src/LambdaClient.ts
@@ -0,0 +1,27 @@
+import * as AWS from 'aws-sdk'
+import { InvocationRequest } from 'aws-sdk/clients/lambda';
+
+export class LambdaClient {
+    private readonly lambda: AWS.Lambda
+
+    constructor(region: string, private readonly arn: string) {
+        this.lambda = new AWS.Lambda({region})
+    }
+
+    async invoke(payload: any): Promise<any> {
+        const req: InvocationRequest = {
+            FunctionName: this.arn,
+            InvocationType: 'RequestResponse', 
+            LogType: 'None',
+            Payload: JSON.stringify(JSON.parse(payload))
+    
+        }
+        const resp = await this.lambda.invoke(req).promise();
+
+        const parsedPayload = JSON.parse((resp.Payload || '{}').toString());
+        if (resp.StatusCode !== 200 || Boolean(resp.FunctionError)) {
+            throw new Error('Lambda invocation (' + this.arn + ') failed: \n' + JSON.stringify(parsedPayload))
+        }
+        return parsedPayload
+    }
+}
